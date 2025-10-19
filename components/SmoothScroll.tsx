c87c0d71160416ffd7a4eee295d62955a10c70a9
tom5n <tomasdocekal15@gmail.com>
2025-10-19 12:08:57 +0200

    Remove invalid direction property from Lenis configuration

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -6,7 +6,6 @@ const SmoothScroll = () => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
       gestureDirection: 'vertical',
       smooth: true,
       mouseMultiplier: 1,
