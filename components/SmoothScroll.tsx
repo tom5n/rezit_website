a07fcc40cde08d6d57b4f29109b54ece707b988e
tom5n <tomasdocekal15@gmail.com>
2025-10-20 19:49:58 +0200

    uprava2

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -6,7 +6,6 @@ const SmoothScroll = () => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true,
       mouseMultiplier: 1,
       smoothTouch: false,
       touchMultiplier: 2,
